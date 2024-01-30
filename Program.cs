00fdf17b24e52af4631ad83125a1ffca325e3854
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2024-01-30 15:55:48 -0500

    feat: Improve hello word message

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
+using System.Net.Mime;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (HttpContext context) =>
+{
+    string html = @"<html>
+                        <body>
+                            <h1> Hello World!</h1>
+                            <br/>
+                            Welcome to this new World!
+                        </body>
+                    </html>";
+    WritenHtml(context, html);
+});
 
 app.Run();
+
+void WritenHtml(HttpContext context, string html)
+{
+    context.Response.ContentType = MediaTypeNames.Text.Html;
+    context.Response.ContentLength = Encoding.UTF8.GetByteCount(html);
+    context.Response.WriteAsync(html);
+}
