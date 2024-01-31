4cdcdd1651abea808b3eac215bea49c181e57ff2
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2024-01-31 15:27:11 -0500

    feat: Pass parameters to action method

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -9,4 +9,19 @@ public class CategoriesController : Controller
     {
         return View();
     }
+    // GET
+    public IActionResult Edit(int? id)
+    {
+        if (id != null)
+        {
+            return new ContentResult { Content = id.ToString() };
+        }
+        else
+        {
+            return new ContentResult { Content = "null content" };
+        }
+
+
+
+    }
 }
