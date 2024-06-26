98a1fb1361b2bee7ca938682b6f6d28feee4e3b7
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2024-06-26 12:15:44 -0400

    feat: Enable use of static files in the app

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.MapControllerRoute(
