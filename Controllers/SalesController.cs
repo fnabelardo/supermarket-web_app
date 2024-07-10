69edf859d3599f6f2a7e63acdc0c71ff4d845eff
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2024-07-10 18:50:09 -0400

    feat: To sell a product
    13.45

--- Controllers/SalesController.cs
+++ Controllers/SalesController.cs
@@ -24,12 +24,26 @@ public class SalesController : Controller
 
     public IActionResult Sell(SalesViewModel salesViewModel)
     {
+        var product = ProductRepository.GetProductById(salesViewModel.SelectedProductId);
+
         if (ModelState.IsValid)
         {
             //Sell the product
+            if (product != null)
+            {
+                TransactionsRepository.Add(
+                    "Cashier1",
+                    salesViewModel.SelectedProductId,
+                    product.Name,
+                    product.Price.HasValue ? product.Price.Value : 0,
+                    product.Quantity.HasValue ? product.Quantity.Value : 0,
+                    salesViewModel.QuantityToSell);
+
+                product.Quantity -= salesViewModel.QuantityToSell;
+                ProductRepository.UpdateProduct(salesViewModel.SelectedProductId, product);
+            }
         }
 
-        var product = ProductRepository.GetProductById(salesViewModel.SelectedProductId);
         salesViewModel.SelectedCategoryId = product?.CategoryId == null ? 0 : product.CategoryId.Value;
         salesViewModel.Categories = CategoriesRepository.GetCategories();
         return View("Index", salesViewModel);
