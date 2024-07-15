853968b0e466973d7626d217329f13ba1102b50d
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2024-07-15 19:19:30 -0400

    feat: Create ICategory repository and ViewCategoriesUseCase
    2.52

---
+++ UseCases/CategoriesUseCases/ViewCategoriesUseCase.cs
@@ -0,0 +1,19 @@
+using CoreBusiness;
+using UseCases.DataStorePluginInterfaces;
+
+namespace UseCases.CategoriesUseCases;
+
+public class ViewCategoriesUseCase
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public ViewCategoriesUseCase(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public IEnumerable<Category> Execute()
+    {
+        return _categoryRepository.GetCategories();
+    }
+}
---
+++ UseCases/DataStorePluginInterfaces/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+using CoreBusiness;
+
+namespace UseCases.DataStorePluginInterfaces;
+
+public interface ICategoryRepository
+{
+    IEnumerable<Category> GetCategories();
+    Category GetCategoryById(int categoryId);
+    void UpdateCategory(int categoryId, Category category);
+}
