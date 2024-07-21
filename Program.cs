c3cc1de3adae40cddd89c04e597abb4ade1d1077
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2024-07-21 13:31:12 -0400

    fix: Fix name of repository and call off AddTransient method

--- Program.cs
+++ Program.cs
@@ -2,10 +2,11 @@ using CoreBusiness;
 using Plugins.DataStore.InMemory;
 using UseCases.CategoriesUseCases;
 using UseCases.DataStorePluginInterfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddSingleton<ICategoryRepository, CategoryInMemoryRepository>;
+builder.Services.AddTransient<ICategoryRepository, CategoriesInMemoryRepository>();
 builder.Services.AddTransient<IViewCategoriesUseCase, ViewCategoriesUseCase>();
 
 var app = builder.Build();
